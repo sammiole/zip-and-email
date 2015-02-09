f43ed2d122f389aa48debcc8d4fd00880e467d2d
Samantha Le <sle273@aucklanduni.ac.nz>
2015-02-09 13:59:39 +1300

    add variables

--- zip_mail.sh
+++ zip_mail.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-# directory containing files to zip
-cd /path_to_directory_containing_files_to_zip/
+# assigning variables, replace with your own
+now=$(date+%m_%d_%Y)
+directory=/path_to_directory_containing_files_to_zip/
+zip_file_name=filename_$now
+recipient=recipient@example.com
+email_body=body_of_your_email.txt
+email_subject='subject line'
 
-# the current date
-now=$(date +%m_%d_%Y)
+# navigate to directory containing files to zip
+cd $directory
 
 # finds the files modified in the last 24 hours (-mtime -1) 
 # for modification time in minutes use -mmin -120 for 120 minutes etc. 
 # creates a .zip of the files in /tmp folder 
 # appends the current time ($now) to the file name
-find . -mtime -1 | xargs zip /tmp/filename_$now
+find . -mtime -1 | xargs zip /tmp/$zip_file_name
 
-# using mutt to send tgz attachment
+# using mutt to send zip attachment
 # fields: subject line, attachment file, recipient email, email text body
-# *********************************
-# -s "subject line"
-# -a /tmp/your_attachment_$now.zip
-# recipient@example.com 
-# < /tmp/body_of_your_email.txt
-# *********************************
-mutt -s 'subject line' -a /tmp/filename_$now.zip -- recipient@email.com < body_of_your_email.txt
+mutt -s $email_subject -a /tmp/$zip_file_name.zip -- $recipient < $email_body
