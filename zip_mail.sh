b7c64d690913f9ed646ebd74284b05d398b72b8b
Samantha Le <sle273@aucklanduni.ac.nz>
2015-02-11 13:16:09 +1300

    update functionality

--- zip_mail.sh
+++ zip_mail.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
-
+ 
+dir=$1
+email=$2
+ 
 # directory containing files to zip
-cd /path_to_directory_containing_files_to_zip/
-
+cd $dir
+ 
 # the current date
 now=$(date +%m_%d_%Y)
 
-# finds the files modified in the last 24 hours (-mtime -1) 
-# for modification time in minutes use -mmin -120 for 120 minutes etc. 
-# creates a .zip of the files in /tmp folder 
-# appends the current time ($now) to the file name
-find . -mtime -1 | xargs zip /tmp/filename_$now
+# check if the directory is not empty
+if ["$(ls $dir)"]; then 
+	
+	# for non-empty files: 
+	# finds the files modified in the last 24 hours (-mtime -1)
+	# for modification time in minutes use -mmin -120 for 120 minutes etc.
+	# creates a .zip of the files in /tmp folder
+	# appends the current time ($now) to the file name
+	find . -mtime -1 ! -name 'thumbs.db' ! -name '.DS_Store' | xargs zip /tmp/filename_$now
+	
+	# using mutt to send tgz attachment
+	# fields: subject line, attachment file, recipient email, email text body
+	# *********************************
+	# -s "subject line"
+	# -a /tmp/your_attachment_$now.zip
+	# recipient@example.com
+	# < /tmp/body_of_your_email.txt 
+	# *********************************
+	mutt -s 'AFA Backup' -a /tmp/filename_$now.zip -- $email < /dev/null
+	
+else
+	# if directory is empty, return "[directory] is empty"
+	echo "$dir is empty"
 
-# using mutt to send zip attachment
-# fields: subject line, attachment file, recipient email, email text body
-# *********************************
-# -s "subject line"
-# -a /tmp/your_attachment_$now.zip
-# recipient@example.com 
-# < /tmp/body_of_your_email.txt
-# *********************************
-mutt -s 'subject line' -a /tmp/filename_$now.zip -- recipient@email.com < body_of_your_email.txt
+fi
