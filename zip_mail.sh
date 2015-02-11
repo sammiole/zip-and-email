5b426de26c108e05d2ba9ff061118dd26d5e3946
Samantha Le <sle273@aucklanduni.ac.nz>
2015-02-11 15:16:45 +1300

    replace [] with test

--- zip_mail.sh
+++ zip_mail.sh
@@ -10,14 +10,14 @@ cd $dir
 now=$(date +%m_%d_%Y)
 
 # check if the directory is not empty
-if ["$(ls $dir)"]; then 
+if test "$(ls $dir)"; then 
 	
 	# for non-empty files: 
 	# finds the files modified in the last 24 hours (-mtime -1)
 	# for modification time in minutes use -mmin -120 for 120 minutes etc.
 	# creates a .zip of the files in /tmp folder
 	# appends the current time ($now) to the file name
-	find . -mtime -1 ! -name 'thumbs.db' ! -name '.DS_Store' | xargs zip /tmp/filename_$now
+	find . -mtime -1 -a ! -name 'Thumbs.db' -a ! -name '.DS_Store' | xargs zip /tmp/filename_$now
 	
 	# using mutt to send tgz attachment
 	# fields: subject line, attachment file, recipient email, email text body
@@ -27,7 +27,7 @@ if ["$(ls $dir)"]; then
 	# recipient@example.com
 	# < /tmp/body_of_your_email.txt 
 	# *********************************
-	mutt -s 'AFA Backup' -a /tmp/filename_$now.zip -- $email < /dev/null
+	mutt -s 'subject line' -a /tmp/filename_$now.zip -- $email < /dev/null
 	
 else
 	# if directory is empty, return "[directory] is empty"
